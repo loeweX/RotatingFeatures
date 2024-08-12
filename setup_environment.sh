7a2f38f94c506fcda9e8a56cd1473a96b48a86fc
Sindy Löwe <loeweX@users.noreply.github.com>
2024-08-12 20:57:11 +0200

    Update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -9,7 +9,7 @@ source activate RotatingFeatures
 
 # Install additional packages.
 conda install pytorch=1.11 torchvision torchaudio cudatoolkit=11.3 -c pytorch
-pip install hydra-core=1.1.0
-pip install einops=0.4.1
+pip install hydra-core==1.1.0
+pip install einops==0.4.1
 pip install timm
 
